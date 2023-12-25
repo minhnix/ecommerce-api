bb864347f53e16e0b66035f8097e20e73cd6d6a8
Minh <bhminh322@gmail.com>
2023-12-25 17:29:36 +0700

    deps: spring data redis

---
+++ src/main/java/com/nix/ecommerceapi/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.nix.ecommerceapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Value("${app.redis.host}")
+    private String redisHost;
+    @Value("${app.redis.port}")
+    private int redisPort;
+    @Value("${app.redis.password}")
+    private String password;
+    @Value("${app.redis.username}")
+    private String username;
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redis = new RedisStandaloneConfiguration(redisHost, redisPort);
+        redis.setUsername(username);
+        redis.setPassword(password);
+        return new LettuceConnectionFactory(redis);
+    }
+
+    @Bean
+    RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+
+}
